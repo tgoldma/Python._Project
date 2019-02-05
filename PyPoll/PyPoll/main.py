a69cd5c6f06e4e51e91bd6177918cd90d1f19fae
tgoldma <46570580+tgoldma@users.noreply.github.com>
2019-02-05 13:44:16 -0600

    Add files via upload

--- PyPoll/PyPoll/main.py
+++ PyPoll/PyPoll/main.py
@@ -1,41 +1,51 @@
 import os
 import csv
 
-csvpath=os.path.join("election_data.csv")
+csvpath = os.path.join("election_data.csv")
 myList = list(csv.reader(open('election_data.csv')))
 
 with open(csvpath) as csvfile:
     csvreader=csv.reader(csvfile, delimiter=',')
     
-    votes=0
-    candidates={}
-    winner=''
-    winningvotes=0
+    votes = 0
+    candidates = {}
+    winner = ''
+    winningvotes = 0
     
-    #loop through rows to make calculations
     csv_header = next(csvreader)
     for row in csvreader:
-        votes+=1
-        #populate dictionary for candidate/votes pair
+        votes += 1
         if row[2] not in candidates:
-            candidates[row[2]]=1
+            candidates[row[2]] = 1
         else:
-            candidates[row[2]]=candidates[row[2]]+1
+            candidates[row[2]] = candidates[row[2]]+1
 
+    for c in candidates:
+        percentage = round((candidates[c]/votes)*100,3)
+        if candidates[c]>winningvotes:
+            winningvotes = candidates[c]
+            winner = c
+    
 output_file = os.path.join("output.txt")
 with open(output_file, 'w') as textfile:
-    textfile.write(f"Election Results \n")
-    textfile.write(f"-------------------------\n")
-    textfile.write(f"Total Votes: {str(votes)}\n")
-    textfile.write(f"-------------------------\n")
-    for c in candidates:
-        percentage=round((candidates[c]/votes)*100,3)
-        textfile.write(f"{c}: {str(percentage)}% ({str(candidates[c])})\n")
-        if candidates[c]>winningvotes:
-            winningvotes=candidates[c]
-            winner=c
-    textfile.write(f"-------------------------\n")
-    textfile.write(f"Winner: {winner} \n")
-    textfile.write(f"-------------------------")
-with open(output_file, 'r') as textfile:
-    print(textfile.read())    
+	    print("Election Results")
+	    textfile.write("Election Results \n")
+	    print("-------------------------")
+	    textfile.write("------------------------- \n")
+	    print(f"Total Votes: {str(votes)}")
+	    textfile.write(f"Total Votes: {str(votes)}"+"\n")
+	    print("-------------------------")
+	    textfile.write("------------------------- \n")
+	    for c in candidates:
+	        percentage = round((candidates[c]/votes)*100,3)
+	        print(f"{c}: {str(percentage)}% ({str(candidates[c])})")
+	        textfile.write(f"{c}: {str(percentage)}% ({str(candidates[c])}) \n")
+	        if candidates[c] > winningvotes:
+	            winningvotes = candidates[c]
+	            winner = c
+	    print("-------------------------")
+	    textfile.write("-------------------------"+"\n")
+	    print(f"Winner: {winner}")
+	    textfile.write(f"Winner: {winner} \n")
+	    print("-------------------------")
+	    textfile.write("-------------------------"+"\n")
